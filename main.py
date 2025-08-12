2fe0b2392dd4f8e53b44d315868c244ab56884c2
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-12 21:47:49 +0800

    Add clipboard
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>

--- main.py
+++ main.py
@@ -23,10 +23,32 @@ app = air.Air(lifespan=http_app.lifespan)
 @app.page
 async def index():
     return air.layouts.mvpcss(
-        air.H1('Hello'),
-        air.P(
-            air.A('MCP', href='/mcp')
-        )
+        air.H1('AirMCP', style='text-align:center'),
+        air.Article(
+            air.Section(
+                air.P('Interact with ', air.A('Air web framework', href='https://airdocs.fastapicloud.dev/'),  ' tools to expedite project development.', style='text-align: center'),
+                air.Aside('https://airmcp.fastapicloud.dev/mcp', class_="copy-on-click"),
+                air.P(air.Small('Click the URL to copy it'), style='text-align: center')
+                )
+        ),
+        air.Children(air.Script("""
+document.querySelectorAll('.copy-on-click').forEach(el => {
+  el.addEventListener('click', () => {
+    const text = el.innerText;
+    const ta = document.createElement('textarea');
+    ta.value = text;
+    ta.style.position = 'fixed';
+    ta.style.left = '-9999px';
+    document.body.appendChild(ta);
+    ta.select();
+    document.execCommand('copy'); // deprecated but widely supported
+    document.body.removeChild(ta);
+    // feedback
+    el.textContent = 'Copied!';
+    setTimeout(() => el.textContent = text, 800);
+  });
+});
+""")),        
     )
 
 app.mount('/mcp', http_app)
