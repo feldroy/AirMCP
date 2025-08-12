795274b5ba436c4ee60c3b18fd461f42f93f754d
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-12 22:17:25 +0800

    Format
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>

--- main.py
+++ main.py
@@ -7,31 +7,47 @@ from fastmcp import FastMCP
 
 mcp = FastMCP("Air")
 
+
 @mcp.tool
 def convert_html_to_airtags(html: str) -> str:
-    'Converts HTML to Air Tags formatted by Ruff'
+    "Converts HTML to Air Tags formatted by Ruff"
     return air_convert.html_to_airtags(html)
+
 
 @mcp.custom_route("/health", methods=["GET"])
 async def health_check(request: Request):
     return JSONResponse({"status": "healthy"})
 
+
 http_app = mcp.http_app(path="/")
 
 app = air.Air(lifespan=http_app.lifespan)
 
+
 @app.page
 async def index():
     return air.layouts.mvpcss(
-        air.H1('AirMCP', style='text-align:center'),
+        air.H1("AirMCP", style="text-align:center"),
         air.Article(
             air.Section(
-                air.P('Interact with ', air.A('Air web framework', href='https://airdocs.fastapicloud.dev/'),  ' tools to expedite project development.', style='text-align: center'),
-                air.Aside('https://airmcp.fastapicloud.dev/mcp', class_="copy-on-click"),
-                air.P(air.Small('Click the URL to copy it'), style='text-align: center')
-                )
+                air.P(
+                    "Interact with ",
+                    air.A(
+                        "Air web framework", href="https://airdocs.fastapicloud.dev/"
+                    ),
+                    " tools to expedite project development.",
+                    style="text-align: center",
+                ),
+                air.Aside(
+                    "https://airmcp.fastapicloud.dev/mcp", class_="copy-on-click"
+                ),
+                air.P(
+                    air.Small("Click the URL to copy it"), style="text-align: center"
+                ),
+            )
         ),
-        air.Children(air.Script("""
+        air.Children(
+            air.Script("""
 document.querySelectorAll('.copy-on-click').forEach(el => {
   el.addEventListener('click', () => {
     const text = el.innerText;
@@ -48,7 +64,9 @@ document.querySelectorAll('.copy-on-click').forEach(el => {
     setTimeout(() => el.textContent = text, 800);
   });
 });
-""")),        
+""")
+        ),
     )
 
-app.mount('/mcp', http_app)
+
+app.mount("/mcp", http_app)
